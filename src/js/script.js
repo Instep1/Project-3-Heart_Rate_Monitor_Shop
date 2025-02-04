981bda158afc8e88a74d7970fb53cbc586f84267
Maksim <lvutinwork@gmail.com>
2025-02-04 13:49:55 +0300

    feat: added section with tabs and section with feedback from clients. Also added addaptive layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,37 +1,44 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick(
-//         {
-//             speed: 1200,
-//             // adaptiveHeight: true,
-//             prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-//             nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-//             responsive: [
-//                 {
-//                     breakpoint: 768,
-//                     settings: {
-//                       dots: true,
-//                       arrows: false
-//                     }
-//                 },
-//             ]
-//         }
-//     );
-//   });   реализация slick slider
+$(document).ready(function(){
+    $('.carousel__inner').slick(
+        {
+            speed: 1200,
+            prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+            responsive: [
+                {
+                    breakpoint: 955,
+                    settings: {
+                      dots: true,
+                      arrows: false
+                    }
+                },
+                {
+                    breakpoint: 360,
+                    settings: {
+                      dots: false,
+                      arrows: false
+                    }
+                },
+            ]
+        }
+    );
 
+    $('ul.catalog__tabs').on('click', 'li:not(catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
 
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
-    nav: false
-  });
+      function toogleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+          })
+      }
 
-document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev'); 
-});
-
-document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next'); 
-});
+      toogleSlide('.catalog-item__link');
+      toogleSlide('.catalog-item__back');
+  }); 
